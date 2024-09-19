015eaad98894e812d2f27ef54cbf8e63c0555929
tomasp92 <tomas@atexto.com>
2024-09-19 01:23:23 -0300

    remove ;

--- processes/email.process.js
+++ processes/email.process.js
@@ -1,20 +1,20 @@
 const { sendEmails } = require('../services')
 require('dotenv').config()
-const Redis = require('ioredis');
-const redisClient = new Redis(process.env.REDIS_URL);
+const Redis = require('ioredis')
+const redisClient = new Redis(process.env.REDIS_URL)
 
 const processQueue = async () => {
   while (true) {
     try {
-      const job = await redisClient.lpop('emailQueue');
+      const job = await redisClient.lpop('emailQueue')
       if (job) {
-        const { file, additionalMessage } = JSON.parse(job);
-        await sendEmails({ file, additionalMessage });
+        const { file, additionalMessage } = JSON.parse(job)
+        await sendEmails({ file, additionalMessage })
       }
     } catch (error) {
-      console.error('Error al procesar la cola:', error);
+      console.error('Error al procesar la cola:', error)
     }
   }
-};
+}
 
 processQueue()
