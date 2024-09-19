1a7908714c3934447dcb800684f3076ff68ee00c
tomasp92 <tomas@atexto.com>
2024-09-19 00:55:53 -0300

    first commit

---
+++ processes/email.process.js
@@ -0,0 +1,19 @@
+const { sendEmails } = require('../services')
+const Redis = require('ioredis');
+const redisClient = new Redis(process.env.REDIS_URL);
+
+const processQueue = async () => {
+  while (true) {
+    try {
+      const job = await redisClient.lpop('emailQueue');
+      if (job) {
+        const { file, additionalMessage } = JSON.parse(job);
+        await sendEmails({ file, additionalMessage });
+      }
+    } catch (error) {
+      console.error('Error al procesar la cola:', error);
+    }
+  }
+};
+
+processQueue()
